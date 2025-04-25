bae8c583d739a718bf0c2a40ae6fe6c115fdd800
Tarun <tarunmalviya8827@gmail.com>
2025-04-25 11:24:27 +0530

    first commit

--- controllers/borrowController.js
+++ controllers/borrowController.js
@@ -1,5 +1,5 @@
 import Book from '../models/book.js';
-import User from '../models/user.js';
+import User from '../models/customer.js';
 
 export const borrowBook = async (req, res) => {
   try {
--- routes/api/books.js
+++ routes/api/books.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import * as bookController from '../../../controllers/bookController.js';
-import Auth from '../../../middleware/JWT_Auth.js';
+import * as bookController from '../../controllers/bookController.js';
+import Auth from '../../middleware/JWT_Auth.js';
 
 const bookRouter = express.Router();
 
-bookRouter.post('/', Auth, bookController.createbook);
-bookRouter.get('/', Auth, bookController.getAllbooks);
+bookRouter.post('/', Auth, bookController.createBook);
+bookRouter.get('/', Auth, bookController.getAllBooks);
 bookRouter.get('/:id', Auth, bookController.getBookById);
-bookRouter.put('/:id', Auth, bookController.updatebook);
-bookRouter.delete('/:id', Auth, bookController.deletebook);
+bookRouter.put('/:id', Auth, bookController.updateBook);
+bookRouter.delete('/:id', Auth, bookController.deleteBook);
 
 export default bookRouter;
