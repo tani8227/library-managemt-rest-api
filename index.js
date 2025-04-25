91f8b9b40058eb2447a77360e4c74f0de05808e4
Tarun <tarunmalviya8827@gmail.com>
2025-04-25 12:02:31 +0530

    change order

--- index.js
+++ index.js
@@ -3,24 +3,20 @@ import dotenv from "dotenv";
 import DB from './config/db.js';
 import routes from './routes/index.js';
 
-
-DB();
 dotenv.config();
+DB();
+
 const port = process.env.PORT;
 const app = express();
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use('/',routes)
+app.use('/', routes);
 
 app.listen(port, (err) => {
     if (err) {
         console.log(`Error in running the server : ${err}`);
-
     }
     console.log(`Server is running at port : ${port}`);
-
-})
+});
